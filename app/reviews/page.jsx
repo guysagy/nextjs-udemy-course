11e3da63011dc66712b0f76506a351ff1141c2ea
Guy Sagy <gsagy@authentic8.com>
2026-09-21 17:57:30 -0700

    section 38

--- app/reviews/page.jsx
+++ app/reviews/page.jsx
@@ -1,31 +1,27 @@
 import  Heading from '@/components/Heading';
+import { getReviews } from '@/lib/reviews';
 import Link from 'next/link';
 
-export default function ReviewsPage() {
+export default async function ReviewsPage() {
+    const reviews = await getReviews();
+
     return (
         <div>
             <Heading>Reviews</Heading>
-            <ul className="flex flex-col gap-3">
-                <li className="bg-white border rounded shadow w-80 hover:shadow-xl">
-                    <Link href="/reviews/hollow-knight">
-                        <img src="/images/hollow-knight.jpg" alt=""
-                            width="320px" height="180px" className="rounded-t"
-                        />
-                        <h2 className="font-orbitron font-semibold py-1 text-center">
-                            Hollow Knight
-                        </h2>
-                    </Link>
-                </li>
-                <li className="bg-white border rounded shadow w-80 hover:shadow-xl">
-                    <Link href="/reviews/stardew-valley">
-                        <img src="/images/stardew-valley.jpg" alt="Stardew Valley"
-                            width="320px" height="180px" className="rounded-t"
-                        />
-                        <h2 className="font-orbitron font-semibold py-1 text-center">
-                            Stardew Valley
-                        </h2>
-                    </Link>
-                </li>
+            <ul className="flex flex-row flex-wrap gap-3">
+                {reviews.map((review) => (
+                    <li key={review.slug}
+                        className="bg-white border rounded shadow w-80 hover:shadow-xl">
+                        <Link href={`/reviews/${review.slug}`}>
+                            <img src={review.image} alt=""
+                                width="320px" height="180px" className="rounded-t"
+                            />
+                            <h2 className="font-orbitron font-semibold py-1 text-center">
+                                {review.title}
+                            </h2>
+                        </Link>
+                    </li>
+                ))}
             </ul>
         </div>
     )
---
+++ content/reviews/hellblade.md
@@ -0,0 +1,6 @@
+---
+title: "Hellblade"
+date: "2023-05-06"
+image: '/images/hellblade.jpg'
+---
+__Hellblade__ Senua's Sacrifice is a 2017 action-adventure game developed and published by Ninja Theory. It is the first installment in the Hellblade series. It was released for PlayStation 4 and Windows in August 2017, Xbox One in April 2018, Nintendo Switch in April 2019, Xbox Series X/S in August 2021, and PlayStation 5 in August 2025. Hellblade features support for virtual reality, which was added in a 2018 update.
